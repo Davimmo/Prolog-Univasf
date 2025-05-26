0025808a4adefdafe1a36e158f062edd7eb4440d
Caio Carvalho <caiocarvalhog001@gmail.com>
2025-05-26 17:53:44 -0300

    quinto, sexto, sétimo periodos

--- univasf.pl
+++ univasf.pl
@@ -17,6 +17,75 @@ coRequesito('lab_eletronica','eletronica_digital').
 coRequesito('OAC','LOAC').
 coRequesito('LOAC','OAC').
 
+
+
+% Quarto Periodo
+
+% Calculo 4
+preRequisito('C2', 'C4').
+
+% Calculo Numero
+preRequisito('C1', 'calc_num').
+preRequisito('algebra', 'calc_num').
+preRequisito('algo-prog', 'calc_Num').
+
+
+% Estatistica
+preRequisito('C1', 'estatistica').
+
+% Eletronica Analogica
+preRequisito('circuitos', 'analogica').
+preRequisito('LEA','LCE').
+
+% Lab. de EA
+preRequisito('circuitos','LEA').
+preRequisito('LCE','LEA').
+
+% Fisica Teorica 3
+preRequisito('fis_2', 'C2').
+
+% POO
+preRequisito('algo_prog','POO').
+
+
+
+
+% Quinto Periodo
+
+preRequisito('C4', 'sinais').
+
+preRequisito('eng-soft','BD').
+
+preRequisito('BD_1','eng-soft1').
+preRequisito('POO','eng-soft1').
+
+preRequisito('OAC','redes').
+preRequisito('LRC','redes').
+
+preRequisito('redes','LRC').
+
+preRequisito('algo_prog','micro').
+preRequisito('analogica','micro').
+preRequisito('LEA','micro').
+preRequisito('OAC','micro').
+
+preRequisito('OAC', 'SO').
+preRequisito('AED', 'SO').
+
+% Sexto Periodo
+
+preRequisito('BD_1','BD_2').
+preRequisito('eng_soft1','eng_soft2').
+preRequisito('micro','IE').
+preRequisito('AED','IA').
+preRequisito('logica','IA').
+preRequisito('algo_prog','LFA').
+preRequisito('discreta','LFA').
+preRequisito('SO','distribuidos').
+preRequisito('redes','distribuidos').
+preRequisito('LRC','distribuidos').
+
+
 ePreRequesito(X, Y) :- 
     preRequesito(X, Y).  % caso base: X é pré-requisito direto de Y
 
